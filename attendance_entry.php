4cf585791f3d755c5f9e473742b05b2c12e57aa8
Dan Bock <danbock@gmail.com>
2015-12-05 10:24:37 -0500

    Attendance entry aesthetic cleanup

--- attendance_entry.php
+++ attendance_entry.php
@@ -56,10 +56,7 @@ function page_content() {
    
    </script>
    
-   <pre><?php
-   global $iqr;
-   print_r($iqr);
-   ?></pre><?php
+   <?php
 
    $qr = pdo_seleqt("
       select
@@ -76,73 +73,95 @@ function page_content() {
          and e.class_source = ?
    ", array($_GET['class_id'], $_GET['class_source']));
    
+   $cqr = seleqt_one_record("
+      select
+         c.class_id,
+         c.start_dttm,
+         c.weeks,
+         u.fname,
+         u.lname
+      from
+         classes_aw c
+         left join wrc_users u
+            on c.instructor_id = u.user_id
+      where
+         c.class_id = ?
+         and c.class_source = ?
+   ", array($_GET['class_id'], $_GET['class_source']));
+
    ?><h2>Attendance Entry</h2>
    
-   <form method="POST" action="attendance_entry.php?class_id=<?php 
-      echo $_GET['class_id'];
-      ?>&class_source=<?php
-      echo $_GET['class_source'];
-      ?>">
-   
-      <table id="attendanceEntry">
-         <tr>
-            <th>
-               Name
-            </th>
-            <?php
-               for($i=1; $i<=$qr[0]['weeks']; $i++) {
-                  ?><th><?php
-                  echo $i;
-                  ?></th><?php
-               }
-            ?>
-         </tr>
-         <?php
-         foreach($qr as $row) {
-            ?><tr><td><?php
-               echo $row['fname'] . ' ' . $row['lname'];
-            ?></td><?php
-               for($j=1; $j<=$qr[0]['weeks']; $j++) {
-                  ?><td id="td_<?php echo $row['user_id'] . '_' . $j ?>">
-                     <!-- Black empty box -->
-                     <a 
-                        href="javascript:submitAttendance(<?php
-                           echo $row['user_id'] . ',' . $j . ',1';
-                        ?>)"
-                        class="blackBox<?php
-                           echo hideClass($row['user_id'], $j, 0);
-                        ?>"
-                     >
-                        <i class="material-icons">&#xE3C1;</i>
-                     </a>
-                     
-                     <img src="spinner.gif" class="hidden" />
+   <table>
+      <tr>
+         <td>
+            <strong>Instructor:</strong>
+         </td>
+         <td>
+            <?php echo $cqr['fname'] . ' ' . $cqr['lname']; ?>
+         </td>
+      </tr>
+      <tr>
+         <td>
+            <strong>Class time:</strong>
+         </td>
+         <td>
+            <?php echo class_times($cqr['start_dttm']); ?>
+         </td>
+      </tr>
+   </table>
 
-                     <!-- Green check -->
-                     <a
-                        href="javascript:submitAttendance(<?php
-                           echo $row['user_id'] . ',' . $j . ',0';
-                        ?>)"
-                        class="greenCheck<?php
-                           echo hideClass($row['user_id'], $j, 1);
-                        ?>"
-                     >
-                        <i class="material-icons">&#xE86C;</i>      
-                     </a>
-                     
-                  </td><?php
-                  
-               }
-            ?></tr><?php
-         }
-         
+   <table id="attendanceEntry">
+      <tr>
+         <th>
+            Name
+         </th>
+         <?php
+            for($i=1; $i<=$qr[0]['weeks']; $i++) {
+               ?><th><?php
+               echo $i;
+               ?></th><?php
+            }
          ?>
-      </table>
-      <input type="hidden" name="formsubmitted" value="true" />
-      <input type="submit" value="Save" />
+      </tr>
+      <?php
+      foreach($qr as $row) {
+         ?><tr><td><?php
+            echo $row['fname'] . ' ' . $row['lname'];
+         ?></td><?php
+            for($j=1; $j<=$qr[0]['weeks']; $j++) {
+               ?><td id="td_<?php echo $row['user_id'] . '_' . $j ?>">
+                  <!-- Black empty box -->
+                  <a
+                     href="javascript:submitAttendance(<?php
+                        echo $row['user_id'] . ',' . $j . ',1';
+                     ?>)"
+                     class="blackBox<?php
+                        echo hideClass($row['user_id'], $j, 0);
+                     ?>"
+                  >
+                     <i class="material-icons">&#xE3C1;</i>
+                  </a>
+
+                  <img src="spinner.gif" class="hidden" />
+
+                  <!-- Green check -->
+                  <a
+                     href="javascript:submitAttendance(<?php
+                        echo $row['user_id'] . ',' . $j . ',0';
+                     ?>)"
+                     class="greenCheck<?php
+                        echo hideClass($row['user_id'], $j, 1);
+                     ?>"
+                  >
+                     <i class="material-icons">&#xE86C;</i>
+                  </a>
+               </td><?php
+            }
+         ?></tr><?php
+      }
       
-      
-   </form>
+      ?>
+   </table>
    
    <?php
 }
