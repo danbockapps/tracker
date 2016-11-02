75e14bd7b16f26cf0e74cbd6a979ebceb187d6f8
Dan Bock <danbock@gmail.com>
2016-11-02 11:43:45 -0400

    Change attendance2 so it doesn't have incentive in DPP.

--- create_or_replace_view_esmmwl.sql
+++ create_or_replace_view_esmmwl.sql
@@ -42,3 +42,58 @@ from
    z_classes c;
 
 source create_or_replace_view.sql;
+
+create or replace view attendance2 as
+select
+   e.tracker_user_id,
+   e.class_id,
+   zc.class_name,
+   e.voucher_code,
+   u.fname,
+   u.lname,
+   coalesce(am.numclasses, 0) as numclasses,
+   e.address1,
+   e.address2,
+   e.city,
+   e.state,
+   e.zip,
+   concat(instrs.fname, " ", instrs.lname) as instructor_name,
+   bw.weight as bw_weight,
+   ew.weight as ew_weight,
+   case
+      when bw.weight > 0 and ew.weight > 0 then "Yes"
+      else "No"
+   end as beginning_and_ending_weight,
+   e.incentive as incentive_type,
+   e.shirtsize,
+   e.shirtcolor,
+   e.birthdate as dob
+from
+   registrants e
+   inner join wrc_users u on
+      e.tracker_user_id = u.user_id
+   natural join classes_aw c
+   left join beginning_weights bw on
+      e.tracker_user_id = bw.user_id and
+      e.class_id = bw.class_id and
+      e.class_source = bw.class_source
+   left join ending_weights ew on
+      e.tracker_user_id = ew.user_id and
+      e.class_id = ew.class_id and
+      e.class_source = ew.class_source
+   left join z_classes zc
+      on e.class_id = zc.id
+   left join wrc_users instrs
+      on c.instructor_id = instrs.user_id
+   left join attendance_sum am on
+      e.tracker_user_id = am.user_id and
+      e.class_id = am.class_id and
+      e.class_source = am.class_source
+where
+   instrs.instructor = 1
+   -- datediff on c.start_dttm was here. Not sure why.
+order by
+   start_dttm desc,
+   lname,
+   fname;
+
