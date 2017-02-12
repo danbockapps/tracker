e179f518dbf66baf9e05b7623c4793ef008eeb82
Dan Bock <danbock@gmail.com>
2017-02-12 08:39:22 -0500

    Add Tracker2 strategies

---
+++ insert_into_wrc_strategies_esmmwl2.sql
@@ -0,0 +1,13 @@
+insert into wrc_strategies (strategy_id, custom, strategy_description) values
+(1, 0, "Eat breakfast"),
+(2, 0, "Eat at least 2 cups of vegetables"),
+(3, 0, "Consume at least 25 grams of fiber"),
+(4, 0, "Sleep at least 7 hours at night"),
+(5, 0, "Consume less than 24 grams of added sugar (women) or 36 grams of added sugar (men)"),
+(6, 0, "Participate in at least one relaxation technique"),
+(7, 0, "Prepare and eat meals at home"),
+(8, 0, "Participate in leisure screen time for 2 hours or less"),
+(9, 0, "Drink 1 or fewer sugar-sweetened beverages"),
+(10, 0, "Participate in at least 30 minutes of physical activity"),
+(11, 0, "Participate in strength training"),
+(12, 0, "Control portion sizes");
