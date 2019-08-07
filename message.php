5866937bde96f7d9132420ba20745c150d508fd6
Dan Bock <danbock@gmail.com>
2019-08-07 00:46:19 -0400

    Update php-cli command and mail functions

--- message.php
+++ message.php
@@ -5,47 +5,7 @@ if(!isset($argv[1]) || !isset($argv[2]) || !isset($argv[3])) {
 }
 else {
    require_once("config.php");
-   require_once('Mail.php');
-   $recipients = $argv[1];
-   if(
-      defined("EMAIL_LOGGER") &&
-      isset($_SERVER['PWD']) &&
-      strpos($_SERVER['PWD'], "/home/esmmwl") !== false
-   ) {
-      $recipients .= "," . EMAIL_LOGGER;
-   }
-   $headers["From"] = EMAIL_FROM;
-   $headers["To"] = $argv[1];
-   $headers["Subject"] = $argv[2];
-   $mailmsg = $argv[3];
-
-   /* SMTP server name, port, user/passwd */
-   $smtpinfo["host"] = "mail.esmmweighless.com";
-   $smtpinfo["port"] = "25";
-   $smtpinfo["auth"] = true;
-   $smtpinfo["username"] = "tracker+esmmweighless.com";
-   $smtpinfo["password"] = EMAIL_PASSWORD;
-
-   /* Suppress mail error messages */
-   $origErrReportingLevel = error_reporting();
-   error_reporting(E_ALL & ~E_STRICT);
-
-   /* Create the mail object using the Mail::factory method */
-   if(isset($ini['sendmail_path'])) {
-      // Works on new server
-      $params['sendmail_path'] = $ini['sendmail_path'];
-      $mail_object =& Mail::factory('sendmail', $params);
-   }
-   else {
-      // Works on old server
-      $mail_object =& Mail::factory("smtp", $smtpinfo);
-   }
-
-   /* Ok send mail */
-   $mail_object->send($recipients, $headers, $mailmsg);
-
-   /* Reset error reporting level */
-   error_reporting($origErrReportingLevel);
+   syncMail($argv[1], $argv[2], $argv[3]);
 }
 
 ?>
--- welcome_email_esmmwl2.php
+++ welcome_email_esmmwl2.php
@@ -1,6 +1,5 @@
 <?php
 require_once("config.php");
-require_once("Mail.php");
 
 $qr = pdo_seleqt("
    select
@@ -36,26 +35,7 @@ foreach($qr as $row) {
 
    $msg = sprintf($format, $row['fname'], $url);
 
-   $recipients = $row['email'];
-   $recipients .= ',danbock@gmail.com';
-   $headers["From"] = EMAIL_FROM;
-   $headers["To"] = $row['email'];
-   $headers["Subject"] = "Eat Smart, Move More, Weigh Less My Dashboard 2";
-
-   /* Suppress mail error messages */
-   $origErrReportingLevel = error_reporting();
-   error_reporting(E_ALL & ~E_STRICT);
-
-   $params['sendmail_path'] = $ini['sendmail_path'];
-   $mail_object =& Mail::factory('sendmail', $params);
-
-   /* Ok send mail */
-   $mail_object->send($recipients, $headers, $msg);
-
-   /* Reset error reporting level */
-   error_reporting($origErrReportingLevel);
-
-
+   syncMail($row['email'], "Eat Smart, Move More, Weigh Less My Dashboard 2", $msg);
 
    $dbh = pdo_connect($ini['db_prefix'] . "_update");
    $sth = $dbh->prepare("
