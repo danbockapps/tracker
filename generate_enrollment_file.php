3fc799ff6e2c8b577ab7f2e5041b6c3a72d94646
Dan Bock <danbock@gmail.com>
2021-08-09 19:13:18 -0400

    Add where clause and trailing pipes

--- generate_enrollment_file.php
+++ generate_enrollment_file.php
@@ -7,17 +7,19 @@ $qr = pdo_seleqt('
       concat(subscriber_id, member_number) as subscriber_id,
       date_format(reg_date, "%m/%d/%Y") as reg_date
    from enrollment_view
+   where voucher_code = "FIBCBSNC"
 ', array());
-
 
 $file = fopen('Value_Based_Benefits_Enrollment_NCSU_' . date('mdY_His') . '.txt', 'w');
 fwrite($file, '1|NCSU|ESMM|' . date('m/d/Y') . "|\n");
 
 foreach($qr as $row) {
-   fputcsv($file, $row, '|');
+   $subscriber_id = $row['subscriber_id'];
+   $reg_date = $row['reg_date'];
+   fwrite($file, "2|$subscriber_id|$reg_date|\n");
 }
 
-fwrite($file, '3|' . count($qr));
+fwrite($file, '3|' . count($qr) . "|\n");
 fclose($file);
 
 ?>
