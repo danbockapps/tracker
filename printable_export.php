dd677963b9b10caf0e4b52d92199707ca91434ba
Dan Bock <danbock@gmail.com>
2021-12-01 10:45:01 -0500

    Add printable_export.php

---
+++ printable_export.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+require_once("config.php");
+
+$qr = current_class_and_sg();
+access_restrict($qr);
+$numWeeks = getNumWeeks($qr['class_id'], $qr['class_source']);
+
+$qr2 = getReports($_GET['user'], $qr['start_dttm']);
+
+foreach($qr2 as $row) {
+  if($row['weight'] != 0) {
+     $reports_empty = false;
+  }
+  $reports['weight'][$row['week_id']-1] = $row['weight'];
+  $reports['aerobic'][$row['week_id']-1] = $row['aerobic_minutes'];
+  $reports['strength'][$row['week_id']-1] = $row['strength_minutes'];
+  $reports['physact'][$row['week_id']-1] = $row['physact_minutes'];
+  $reports['avgsteps'][$row['week_id']-1] = $row['avgsteps'];
+  $reports['notes'][$row['week_id']-1] = $row['notes'];
+}
+
+
+for($i=0; $i<$numWeeks; $i++) {
+  ?>
+  <h2><?= wrcdate($qr['start_dttm'] . " + $i weeks") ?></h2>
+  <p>
+    Weight: <?= $reports['weight'][$i] ?><br/>
+    Aerobic minutes: <?= $reports['aerobic'][$i] ?><br/>
+    Strength minutes: <?= $reports['strength'][$i] ?><br/>
+    Physical activity minutes: <?= $reports['physact'][$i] ?><br/>
+    Average steps: <?= round($reports['avgsteps'][$i], 0) ?><br/>
+    Notes: <?= $reports['notes'][$i] ?><br/>
+  </p>
+  <?php
+}
+
+?>
