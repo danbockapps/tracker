1362d112e92156148a69037c9479801e56762bc4
Dan Bock <danbock@gmail.com>
2021-12-15 08:29:32 -0500

    Comment out full_participation

--- view_report.php
+++ view_report.php
@@ -41,6 +41,7 @@ if($_GET['report'] == "attendance3") {
          numclasses,
          numclasses_phase1,
          numclasses_phase2,
+         -- full_participation, // This isn't in attendance3?!
          address1,
          address2,
          city,
