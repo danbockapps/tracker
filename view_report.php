c2b6397da261be6c67143157632493cf27549c7e
Dan Bock <danbock@gmail.com>
2022-01-12 09:28:34 -0500

    Add back full_participation

--- view_report.php
+++ view_report.php
@@ -41,7 +41,7 @@ if($_GET['report'] == "attendance3") {
          numclasses,
          numclasses_phase1,
          numclasses_phase2,
-         -- full_participation, // This isn't in attendance3?!
+         full_participation,
          address1,
          address2,
          city,
