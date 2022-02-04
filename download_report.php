230e6eaec8d54f2207f7e7ec1927527d1a41529c
Dan Bock <danbock@gmail.com>
2022-02-04 06:55:52 -0500

    Add user IDs to report scripts

--- download_report.php
+++ download_report.php
@@ -79,6 +79,8 @@ else if($_GET['report'] == "attendance3") {
    require_get_vars("class");
    $qr = pdo_seleqt("
       select
+         tracker_user_id,
+         admin_db_user_id,
          instructor_name,
          class_name,
          voucher_code,
