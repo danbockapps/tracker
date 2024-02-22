6a49d11abdf8a5adcf63740889211490fcbdf632
Dan Bock <danbock@gmail.com>
2024-02-22 09:17:39 -0500

    HTML email for WL2

--- emailTemplates.php
+++ emailTemplates.php
@@ -449,15 +449,84 @@ EOD;
 }
 
 function welcomeEmailWl2($fname, $email, $activation) {
-  $format = file_get_contents('welcome_esmmwl2.txt');
+  global $head, $img;
+  $setPwUrl = WEBSITE_URL . "/setpw.php?email=" . urlencode($email) . "&key=" . $activation;
+  $websiteUrl = WEBSITE_URL;
 
-  return sprintf(
-    $format,
-    $fname,
-    $activation == null ?
-      WEBSITE_URL . "/setpw.php?email=" . urlencode($email) . "&key=" . $activation :
-      WEBSITE_URL
-  );
+  $str = <<<EOD
+<!DOCTYPE html>
+<html>
+$head
+<body>
+  $img
+
+  <p style="font-size: 12px">
+    Hello %s,
+  </p>
+
+  <p style="font-size: 12px">
+    We hope you enjoyed your first Eat Smart, Move More, Weigh Less2 class.
+  </p>
+
+  <p style="font-size: 12px">
+    As you heard from your instructor, the next step in your Eat Smart, Move More, Weigh Less2 journey is to get started using My Dashboard 2. Tracking your progress and engaging with your instructor using My Dashboard 2 is essential to your success in the program. Each week, you will use it to record your: 1) weight, 2) minutes of aerobic activity or steps per day, and 3) minutes of strength training activity. During weeks #1 and #12, you will record your measurements for: 1) blood pressure and 2) waist circumference. Your week #1 report will also collect your height in order to calculate your BMI.
+  </p>
+
+  <p style="font-size: 12px">
+    To activate your My Dashboard 2 account:
+    <ol>
+      <li>
+        <a class="btn btn-big-margin" style="color: #ffffff" href="$websiteUrl">
+          My Dashboard
+        </a>
+      </li>
+      <li>If you don't already have a My Dashboard 2 password, create one; you will use this email address and the password you create to login to My Dashboard 2.</li>
+      <li>You can now login to your My Dashboard 2 account. Please make sure to bookmark the site so you can access easily it in the future. You can also find it through the My Dashboard 2 button on our website home page, www.esmmweighless.com. If you forget your password, you can click on the link on the login page to have your password emailed to you.</li>
+    </ol>
+  </p>
+
+  <p style="font-size: 12px">
+    Enter your weekly numbers
+  </p>
+
+  <p style="font-size: 12px">
+    Each week, click on the date representing the current week (in green) to enter your: 1) weight, 2) total minutes of aerobic activity, and 3) total minutes of strength training activity. The minutes of activity you enter should be based on the previous seven days, but the weight you enter should represent your current weight.
+  </p>
+
+  <p style="font-size: 12px">
+    During weeks #1 and #12 only, you will record your measurements for: 1) blood pressure and 2) waist circumference. Please refer to the instructions that you received in your packet when taking these measurements.
+  </p>
+
+  <p style="font-size: 12px">
+    Communicate with Your Instructor
+  </p>
+
+  <p style="font-size: 12px">
+    You can communicate at any time with your instructor by clicking on the green "Messages" tab at the top of the screen. When your instructor responds, you will receive notification in this email inbox. Please use this tool to ask your instructor questions or get feedback about your experience in the program.
+  </p>
+
+  <p style="font-size: 12px">
+    Optional Reflection on Healthy Strategies
+  </p>
+
+  <p style="font-size: 12px">
+    Feel free to use this list to reflect on how many days you adopted recommended strategies during the past week. Let your instructor know if you want feedback on your entries in this section.
+  </p>
+
+  <p style="font-size: 12px">
+    SMART Goal
+  </p>
+
+  <p style="font-size: 12px">
+    After week #1, you will enter a NEW SMART goal. During the first class, your instructor will provide detailed guidance on setting your SMART goal, an essential component of your Eat Smart, Move More, Weigh Less2 experience.
+  </p>
+
+  <p style="font-size: 12px">
+    Please email administrator@esmmweighless.com if you have issues setting up your My Dashboard 2 account.
+  </p>
+EOD;
+
+  return $str;
 }
 
 ?>
