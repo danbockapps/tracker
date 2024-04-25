ed5a4e2345356660609667ae04cc2c1c861e61bf
Dan Bock <danbock@gmail.com>
2024-04-25 18:21:45 -0400

    Aesthetic updates

--- attendance_class_list.php
+++ attendance_class_list.php
@@ -14,32 +14,33 @@ function page_content() {
    }
    
    ?>
-   <h2>Attendance Entry for <?php echo full_name($_GET['instr']); ?></h2>
-   <h3>Select a class</h3>
-   <ul id="classList"><?php
+   <div class="fade">
+      <h2>Attendance Entry for <?php echo full_name($_GET['instr']); ?></h2>
+      <h3>Select a class</h3>
+      <ul id="classList"><?php
 
-   $qr = pdo_seleqt("
-      select
-         class_id,
-         start_dttm
-      from classes_aw
-      where instructor_id = ?
-      order by start_dttm desc
-   ", $_GET['instr']);
-   
-   foreach($qr as $row) {
-      ?><li><a href="attendance_entry<?php
-         if(PRODUCT == 'dpp') {
-            echo '_detailed';
-         }
-      ?>.php?class_id=<?php
-         echo $row['class_id'];
-      ?>"><?php
-         echo class_times($row['start_dttm']);
-      ?></a></li><?php
-   }
+      $qr = pdo_seleqt("
+         select
+            class_id,
+            start_dttm
+         from classes_aw
+         where instructor_id = ?
+         order by start_dttm desc
+      ", $_GET['instr']);
 
-   ?></ul><?php
+      foreach($qr as $row) {
+         ?><li><a href="attendance_entry<?php
+            if(PRODUCT == 'dpp') {
+               echo '_detailed';
+            }
+         ?>.php?class_id=<?php
+            echo $row['class_id'];
+         ?>"><?php
+            echo class_times($row['start_dttm']);
+         ?></a></li><?php
+      }
+
+   ?></ul></div><?php
 
 }
 
